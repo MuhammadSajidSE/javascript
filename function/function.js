a5b039fd40d436b6fab92ca25ce869e0541cfeb4
Muhammad Sajid <muhammadsajid.dh@gmail.com>
2024-12-28 09:34:20 +0500

    javascript commit

---
+++ function/function.js
@@ -0,0 +1,11 @@
+console.log("I am going to using function");
+function student(name,roll,CGP) {
+    var salam="Asalam walikum";
+    console.log(salam);
+    console.log("My name is " + name);
+    console.log("My roll number is " + roll);
+    console.log("My CGP is " + CGP);}
+student("sajid","SE-22033",3.7);
+student("saad","SE-22072",3.7);
+student("mehveen","SE-22012",3.46);
+console.log(salam);
